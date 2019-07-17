87d29eec6246b9794ebaa830add297074ee30366
mbercx <marnik.bercx@uantwerpen.be>
2019-07-17 17:16:32 +0200

    testing: parallelization script in workflow

--- vscworkflows/workflow/firetasks.py
+++ vscworkflows/workflow/firetasks.py
@@ -132,16 +132,19 @@ class VaspParallelizationTask(FiretaskBase):
         # Get the number of k-points
         with open(stdout_file, 'w') as f_std, \
                 open(stderr_file, "w", buffering=1) as f_err:
-            subprocess.Popen(vasp_cmd, stdout=f_std, stderr=f_err)
+            p = subprocess.Popen(vasp_cmd, stdout=f_std, stderr=f_err)
 
-         while not os.path.exists(os.path.join(self["directory"], "IBZKPT")):
+        while not os.path.exists(os.path.join(self["directory"], "IBZKPT")):
             time.sleep(10)
 
         with open(os.path.join(self["directory"], "IBZKPT"), "r") as file:
-            number_of_kpoints =  int(file.read().split('\n')[1])
+            number_of_kpoints = int(file.read().split('\n')[1])
 
         with open(os.path.join(self["directory"], "STOPCAR"), "w") as file:
             file.write("LABORT=True")
+
+        while os.path.exists(os.path.join(self["directory"], "STOPCAR")):
+            time.sleep(10)
 
         # Get the total number of cores
         try:
