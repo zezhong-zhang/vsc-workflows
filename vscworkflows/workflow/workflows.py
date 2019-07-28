f5fd67fda32b8aecbf511e6cfb77cca0874e3c6d
mbercx <marnik.bercx@uantwerpen.be>
2019-07-28 14:03:10 +0200

    fix: get_wf_optics() get_wf_optics wasn't constructed properly: instead of being a child of the optimize firework, the optics firework simply took the initial structure, which wasn't updated from the addition of an optimize fw. This should now be fixed.

--- vscworkflows/workflow/workflows.py
+++ vscworkflows/workflow/workflows.py
@@ -201,7 +201,7 @@ def get_wf_optics(structure, directory, functional=("pbe", {}), k_resolution=Non
     Starts with a geometry optimization.
 
     Args:
-        structure: pymatgen.Structure OR path to the structure file.
+        structure (Structure): Input geometry.
         directory (str): Directory in which the optics calculation should be
             performed.
         functional (tuple): Tuple with the functional details. The first element
@@ -265,7 +265,7 @@ def get_wf_optics(structure, directory, functional=("pbe", {}), k_resolution=Non
 
     # Set up the geometry optimization Firework
     optics_fw = OpticsFW(
-        structure=structure,
+        parents=optimize_fw,
         vasp_input_params=vasp_input_params,
         in_custodian=in_custodian,
         spec=spec
@@ -285,7 +285,7 @@ def get_wf_slab_optimize(slab, directory, user_slab_settings,
                          functional=("pbe", {}), is_metal=False,
                          in_custodian=False, number_nodes=None):
     """
-    Set up a geometry optimization workflow.
+    Set up a slab geometry optimization workflow.
 
     Args:
         slab (Qslab): Slab for which to set up the geometry optimization workflow.
@@ -341,7 +341,7 @@ def get_wf_slab_optimize(slab, directory, user_slab_settings,
 def get_wf_slab_dos(slab, directory, functional=("pbe", {}), k_resolution=0.1,
                     calculate_locpot=False, in_custodian=False, number_nodes=None):
     """
-    Set up a geometry optimization workflow.
+    Set up a slab DOS workflow. Starts with a geometry optimization.
 
     Args:
         slab (Qslab): Slab for which to set up the DOS workflow.
@@ -400,104 +400,81 @@ def get_wf_slab_dos(slab, directory, functional=("pbe", {}), k_resolution=0.1,
     return Workflow(fireworks=[dos_fw, ], name=workflow_name)
 
 
-def get_wf_quotas(bulk, slab_list, directory, functional=("pbe", {}),
-                  base_k_resolution=0.1, is_metal=False,
-                  in_custodian=False, number_nodes=None):
-    """
-    Generate a full QUOTAS worfklow, i.e. one that:
-
-    1. Optimizes the bulk and calculates the optical properties.
-    2. Optimizes the slabs in 'slab_list' and then calculates the DOS and work function.
-
-    Args:
-        bulk:
-        slab_list:
-        functional:
-        base_k_resolution:
-        number_nodes:
-
-    """
-
-    optics_k_resolution = base_k_resolution / 3
-    dos_k_resolution = base_k_resolution / 2
-
-    # Set up the directories for the bulk calculations
-    bulk_optimize_dir = _set_up_relative_directory(
-        directory=os.path.join(directory, "bulk"),
-        functional=functional,
-        calculation="optimize"
-    )
-    bulk_optics_dir = _set_up_relative_directory(
-        directory=os.path.join(directory, "bulk"),
-        functional=functional,
-        calculation="optics"
-    )
-
-    bulk_optics = OpticsFW(
-        structure=os.path.join(bulk_optimize_dir, "final_structure.json"),
-        directory=bulk_optics_dir,
-        functional=functional,
-        k_resolution=optics_k_resolution,
-        is_metal=is_metal,
-        in_custodian=in_custodian,
-        number_nodes=number_nodes
-    )
-
-    bulk_optimize = OptimizeFW(
-        structure=bulk,
-        functional=functional,
-        directory=bulk_optimize_dir,
-        is_metal=is_metal,
-        in_custodian=in_custodian,
-    )
-
-    fireworks = [bulk_optimize]
-    links_dict = {}
-
-    for slab_dict in slab_list:
-        # Set up the directories for the slab calculations
-
-        slab_dir = str(slab_dict["slab"].miller_index).strip("()").replace(", ", "")
-
-        slab_optimize_dir = _set_up_relative_directory(
-            directory=os.path.join(directory, slab_dir),
-            functional=functional,
-            calculation="optimize"
-        )
-
-        slab_dos_dir = _set_up_relative_directory(
-            directory=os.path.join(directory, slab_dir),
-            functional=functional,
-            calculation="dos"
-        )
-
-        slab_optimize = SlabOptimizeFW(
-            slab=slab_dict["slab"],
-            directory=slab_optimize_dir,
-            fix_part="center",
-            fix_thickness=slab_dict["fix_thickness"],
-            functional=functional,
-            is_metal=is_metal,
-            in_custodian=in_custodian,
-            number_nodes=number_nodes
-        )
-
-        slab_dos = SlabDosFW(
-            slab=os.path.join(slab_optimize_dir, "final_slab.json"),
-            directory=slab_dos_dir,
-            functional=functional,
-            k_resolution=dos_k_resolution,
-            calculate_locpot=True,
-            in_custodian=in_custodian,
-            number_nodes=number_nodes
-        )
-
-        fireworks.extend([slab_optimize, slab_dos])
-        links_dict.update({slab_optimize: [slab_dos]})
-
-    # Set up a clear name for the workflow
-    workflow_name = str(bulk.composition.reduced_formula).replace(" ", "")
-    workflow_name += " - QUOTAS - "
-    workflow_name += " " + str(functional)
-
-    return Workflow(fireworks=fireworks, links_dict=links_dict, name=workflow_name)
+# def get_wf_quotas(bulk, slab_list, directory, functional=("pbe", {}),
+#                   base_k_resolution=0.1, is_metal=False,
+#                   in_custodian=False, number_nodes=None):
+#     """
+#     Generate a full QUOTAS worfklow, i.e. one that:
+#
+#     1. Optimizes the bulk and calculates the optical properties.
+#     2. Optimizes the slabs in 'slab_list' and then calculates the DOS and work
+#     function.
+#
+#     Args:
+#         bulk:
+#         slab_list:
+#         functional:
+#         base_k_resolution:
+#         number_nodes:
+#
+#     """
+#
+#     optics_k_resolution = base_k_resolution / 3 # TODO Improve this
+#     dos_k_resolution = base_k_resolution / 2
+#
+#     # Set up the directory for the bulk calculations
+#     bulk_dir = os.path.join(directory, "bulk")
+#
+#     optics_wf = get_wf_optics(
+#         directory=directory, structure=bulk, functional=functional,
+#         k_resolution=optics_k_resolution, is_metal=is_metal,
+#         in_custodian=in_custodian, number_nodes=number_nodes
+#     )
+#
+#     for slab_dict in slab_list:
+#         # Set up the directories for the slab calculations
+#
+#         slab_dir = str(slab_dict["slab"].miller_index).strip("()").replace(", ", "")
+#
+#         slab_optimize_dir = _set_up_relative_directory(
+#             directory=os.path.join(directory, slab_dir),
+#             functional=functional,
+#             calculation="optimize"
+#         )
+#
+#         slab_dos_dir = _set_up_relative_directory(
+#             directory=os.path.join(directory, slab_dir),
+#             functional=functional,
+#             calculation="dos"
+#         )
+#
+#         slab_optimize = SlabOptimizeFW(
+#             slab=slab_dict["slab"],
+#             directory=slab_optimize_dir,
+#             fix_part="center",
+#             fix_thickness=slab_dict["fix_thickness"],
+#             functional=functional,
+#             is_metal=is_metal,
+#             in_custodian=in_custodian,
+#             number_nodes=number_nodes
+#         )
+#
+#         slab_dos = SlabDosFW(
+#             slab=os.path.join(slab_optimize_dir, "final_slab.json"),
+#             directory=slab_dos_dir,
+#             functional=functional,
+#             k_resolution=dos_k_resolution,
+#             calculate_locpot=True,
+#             in_custodian=in_custodian,
+#             number_nodes=number_nodes
+#         )
+#
+#         fireworks.extend([slab_optimize, slab_dos])
+#         links_dict.update({slab_optimize: [slab_dos]})
+#
+#     # Set up a clear name for the workflow
+#     workflow_name = str(bulk.composition.reduced_formula).replace(" ", "")
+#     workflow_name += " - QUOTAS - "
+#     workflow_name += " " + str(functional)
+#
+#     return Workflow(fireworks=fireworks, links_dict=links_dict, name=workflow_name)
