61ba623f062d1245eb02113dfd3074d41451ff65
mbercx <marnik.bercx@uantwerpen.be>
2019-07-30 09:45:59 +0200

    testing: issue with child fireworks in WriteVaspFromIOSet

--- vscworkflows/firetasks/core.py
+++ vscworkflows/firetasks/core.py
@@ -357,7 +357,7 @@ class WriteVaspFromIOSet(FiretaskBase):
                                           **self.get("vasp_input_params", {}))
             elif "parents" in self.keys():
                 with open("temp.txt", "w") as file:
-                    file.write(str(print(self["parents"].as_dict())))
+                    file.write(str(print(self["parents"])))
 
                 try:
                     parent_dir = self["parents"]["spec"]["_launch_dir"]
