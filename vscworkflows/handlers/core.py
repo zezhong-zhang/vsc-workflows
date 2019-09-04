bea7010f6afeff199415b8a4875521e7cf1fc3d9
mbercx <marnik.bercx@uantwerpen.be>
2019-09-04 15:49:00 +0200

    add: MemoryMonitorHandler
    Added a second simple ErrorHandler that fixes the memory issues I'm running into when using too many k-points for an optical calculation. This one monitors the memory usage and tries to correct if necessary. This might not work in case the memory spike occurs in between two monitoring times.

--- vscworkflows/handlers/core.py
+++ vscworkflows/handlers/core.py
@@ -2,6 +2,8 @@
 # Copyright (c) Marnik Bercx, University of Antwerp
 # Distributed under the terms of the MIT License
 
+import subprocess
+
 from pymatgen.io.vasp.inputs import Kpoints
 
 from custodian.custodian import ErrorHandler
@@ -61,3 +63,44 @@ class MemoryErrorHandler(ErrorHandler):
                            for k in kpoints.kpts[0]]
         kpoints.write_file("KPOINTS")
 
+
+class MemoryMonitorHandler(ErrorHandler):
+    """
+    Monitor the memory of a job and reduce k-points if needed.
+
+    """
+
+    is_monitor = True
+
+    def __init__(self, output_filename="vasp.out",
+                 min_free_memory=1e3, kpoints_multiplier=0.95):
+        """
+        Initializes the handler with the output file to check.
+
+        Args:
+            output_filename (str): Filename for the stdout.
+            kpoints_multiplier (float): Number < 1 which to multiply the number
+                of k-points with when a memory issue is detected.
+
+        """
+        self.output_filename = output_filename
+        self.kpoints_multiplier = kpoints_multiplier
+        self.min_free_memory = min_free_memory
+        self.errors = set()
+
+    def check(self):
+
+        result = subprocess.check_output(['bash', '-c', 'free -m'])
+        free_memory = result.split('\n')[1].split()[3]
+
+        with open("memory.log", "a+") as file:
+            file.write(str(free_memory) + " MB.")
+
+        return free_memory < self.min_free_memory
+
+    def correct(self):
+
+        kpoints = Kpoints.from_file("KPOINTS")
+        kpoints.kpts[0] = [int(self.kpoints_multiplier * k)
+                           for k in kpoints.kpts[0]]
+        kpoints.write_file("KPOINTS")
