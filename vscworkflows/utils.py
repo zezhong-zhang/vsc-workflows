895498f637f282242b57a33109bc6f0a32d9a4a3
mbercx <marnik.bercx@uantwerpen.be>
2019-10-26 23:15:44 +0200

    add: vasp_input_update
    Added quick utility function that updates a dictionary that details the VASP input parameters to be passed to the DictSet (sub)class. Simply updating doesn't work because e.g. the user_incar_settings dictionary should usually not be entirely overwritten by the updated setting in case the user specifies a single INCAR tag. However, the user_kpoints_settings _should_ be overwritten entirely, as in case the Firework has a default user_kpoints_setting - which the user wants to overwrite with vasp_input_params - adding two keywords that define the kpoint generation scheme creates issues. This is due to the fact that DictSet goes down a list of options and simply chooses the first keyword that it finds in the dictionary. In other words, it's possible that the user chosen setting is ignored in case the default setting is first in line.

---
+++ vscworkflows/utils.py
@@ -0,0 +1,27 @@
+# coding: utf8
+# Copyright (c) Marnik Bercx, University of Antwerp
+# Distributed under the terms of the MIT License
+
+import collections
+
+"""
+Utility methods for the vsc-workflows package.
+
+"""
+
+__author__ = "Marnik Bercx"
+__copyright__ = "Copyright 2019, Marnik Bercx, University of Antwerp"
+__version__ = "pre-alpha"
+__maintainer__ = "Marnik Bercx"
+__email__ = "marnik.bercx@uantwerpen.be"
+__date__ = "Oct 2019"
+
+
+def vasp_input_update(vasp_input_params, d):
+
+    for k, v in d.items():
+        if isinstance(v, dict) and k != "user_kpoints_settings":
+            vasp_input_params[k].update(v)
+        else:
+            vasp_input_params[k] = v
+    return vasp_input_params
