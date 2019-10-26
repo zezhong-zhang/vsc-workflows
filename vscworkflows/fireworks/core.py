ad9a487d22bcdb031a0cf876c37d0775a3d0da37
mbercx <marnik.bercx@uantwerpen.be>
2019-10-27 01:10:28 +0200

    change: fix charge density for DOS run in SlabDosFW

--- vscworkflows/fireworks/core.py
+++ vscworkflows/fireworks/core.py
@@ -407,7 +407,8 @@ class SlabDosFW(Firework):
 
         # Default input parameters for actual DOS run
         dos_input_params = {
-            "user_incar_settings": {"NEDOS": 2000, "EDIFF": 1.0e-6, "ICHARG": 1},
+            "user_incar_settings": {"NEDOS": 2000, "EDIFF": 1.0e-6,
+                                    "ICHARG": 11},
             "user_kpoints_settings": {"k_resolution": 0.05}
         }
 
